365807cd31d2964f81261238d01ef88e077797cc
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-05-04 11:56:40 -0300

    Add script to create Kubernetes cluster with kind

---
+++ s3-files-access/create-poc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install Pre Requisites
+
+# On Linux:
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# On MacOS:
+# For Intel Macs
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-darwin-amd64
+# For M1 / ARM Macs
+[ $(uname -m) = arm64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-darwin-arm64
+chmod +x ./kind
+mv ./kind /some-dir-in-your-PATH/kind
+
+# Create Kubernetes Cluster
+
+kind create cluster --config=kind-config.yaml
